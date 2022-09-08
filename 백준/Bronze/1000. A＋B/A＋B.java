8ad8ed10a7c04d93e7d303a66a94536faebfbe03
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2022-09-08 12:11:23 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner ab = new Scanner(System.in);
+            int a = ab.nextInt();
+            int b = ab.nextInt();
+        System.out.println(a+b);
+    }
+}
