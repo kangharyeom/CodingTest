e361c783641b5ef6f08cef2f652774a78c232798
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2022-09-11 00:06:30 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args) {
+        Scanner ab = new Scanner(System.in);
+        String a = ab.next();
+        System.out.println(a + "??!");
+    }
+}
