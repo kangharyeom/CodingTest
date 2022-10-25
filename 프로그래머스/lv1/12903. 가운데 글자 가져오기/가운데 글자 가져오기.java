93209dc0115adf62467379914ce5e636382538de
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2022-10-25 23:44:26 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 11.83 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String a = "";
+
+        if(s.length()%2!=0){
+           return String.valueOf(s.charAt(s.length()/2));
+        }
+        if(s.length()%2==0){
+            for (int i=s.length()/2-1; i<s.length()/2+1; i++){
+             answer += s.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
