fe54a9895dbd039f565fb8b61bb49566f59c4497
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2022-12-06 23:41:30 +0900

    [Bronze II] Title: 단어의 개수, Time: 468 ms, Memory: 30428 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner str = new Scanner(System.in);
+
+        String string = str.nextLine();
+        str.close();
+
+        StringTokenizer st = new StringTokenizer(string, " ");
+        System.out.println(st.countTokens());
+    }
+}
