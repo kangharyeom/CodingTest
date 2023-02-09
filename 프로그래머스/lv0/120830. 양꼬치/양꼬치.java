c537d0fe02f6ba56515d79ef977bed810d985a31
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2023-02-09 23:51:30 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        if (n<10){
+        answer = (n*12000)+(k*2000);
+        } else if(n>=10){
+            int num = n/10;
+            answer = (n*12000)+(k*2000)-(num*2000);
+        }
+        return answer;
+    }
+}
