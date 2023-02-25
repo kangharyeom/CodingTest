2f8c4111d31f356b31c3034df7c7dfef203687a8
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2023-02-25 23:49:25 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.30 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string) {
+      int answer = 0;
+        
+        String replace = "";
+		replace = my_string.toLowerCase().replaceAll("[abcdefghijklmnopqrstuvwxyz]", "");
+		String[] arry = replace.split("");
+
+            for(int i = 0; i< arry.length; i++) {
+                answer += Integer.parseInt(arry[i]);
+            }
+
+        
+        return answer;
+    }
+}
