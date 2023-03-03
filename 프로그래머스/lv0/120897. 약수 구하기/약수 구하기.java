c8aa4f06f1bc3f3675dc241d711e4738ee0a033d
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2023-03-03 16:55:32 +0900

    [level 0] Title: 약수 구하기, Time: 1.96 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        for (int i=1; i<=n; i++){
+            if(n%i==0){
+                arr.add(i);
+            }
+        }
+        int[] answer = arr.stream().mapToInt(i->i).toArray();
+        
+        return answer;
+    }
+}
