7824883d0c661aee1e2fbab1fed3455b179ab314
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2023-04-04 13:48:18 +0900

    [level 1] Title: 3진법 뒤집기, Time: 15.77 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,10 +1,8 @@
-import java.util.*;
 class Solution {
     public int solution(int n) {
         String a = "";
 
         while(n>0){
-            // a += Integer.toString(n%3);
             a += (n%3);
             n /=3;
         }
