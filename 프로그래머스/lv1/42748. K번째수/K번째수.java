2736882c858ab678221700b43cafe055ae5bef57
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2023-05-03 23:58:49 +0900

    [level 1] Title: K번째수, Time: 0.33 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -4,10 +4,10 @@ class Solution {
 		int[] answer = new int[commands.length];
 
 		for (int i = 0; i < commands.length; i++) {
-			int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]); 
+			int[] arrInt = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]); 
 
-			Arrays.sort(temp); 
-			answer[i] = temp[commands[i][2] - 1];
+			Arrays.sort(arrInt); 
+			answer[i] = arrInt[commands[i][2] - 1];
 		}
 		return answer;
 	}
