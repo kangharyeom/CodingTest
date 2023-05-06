880130c32650639e0b44838ae0c58140d2823c3c
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2023-05-06 23:53:17 +0900

    [unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public ArrayList<Integer> solution(int start, int end) {
+        ArrayList<Integer> arrStr = new ArrayList<>();
+        for (int i=start; i<=end; i++){
+            arrStr.add(i);
+        }
+        return arrStr;
+    }
+}
