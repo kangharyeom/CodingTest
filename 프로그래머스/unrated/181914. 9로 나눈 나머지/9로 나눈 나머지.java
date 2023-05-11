a28349c4e121eb480d6519fb155731eb458c4960
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2023-05-11 19:50:20 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        for(int i=0; i<number.length(); i++){
+            char a = number.charAt(i);
+            answer+= Integer.valueOf(a-'0');
+        }
+        return answer%9;
+    }
+}
