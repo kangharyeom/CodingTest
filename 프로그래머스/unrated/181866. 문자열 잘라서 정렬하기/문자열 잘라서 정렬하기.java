15056264cf6cb14c626288ba9ea1fb70bf3c8f66
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2023-05-23 11:50:46 +0900

    [unrated] Title: 문자열 잘라서 정렬하기, Time: 18.25 ms, Memory: 92.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public ArrayList<String> solution(String myString) {
+        String[] arr = myString.split("[x]");
+        Arrays.sort(arr);
+        ArrayList<String> answer = new ArrayList<>();
+        
+        for(int i=0; i<arr.length; i++){
+            if(!arr[i].equals("")){
+                answer.add(arr[i]);
+            }
+        }
+        
+        return answer;
+    }
+}
