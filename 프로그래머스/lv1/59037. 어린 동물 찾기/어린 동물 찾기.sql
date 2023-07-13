9e107f13412b61175fbdd68d40ce8bea5c366663
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2023-07-13 20:12:05 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/lv1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,4 +1,4 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID,	NAME
-from ANIMAL_INS
-where INTAKE_CONDITION <> 'Aged';
+select ANIMAL_ID, NAME
+from animal_ins
+where INTAKE_CONDITION != 'Aged'
+order by animal_id asc
