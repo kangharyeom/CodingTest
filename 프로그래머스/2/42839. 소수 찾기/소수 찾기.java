34f6236985317acb99e21c996c025476fe0580ab
Kangharyeom <kangharyeom@gmail.com>
2024-08-23 12:32:01 +0900

    [level 2] Title: 소수 찾기, Time: 32.50 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+    HashSet<Integer> numbersSet = new HashSet<>();
+    public int solution(String numbers) {
+        dfs("", numbers);
+
+        int count = 0;
+        Iterator<Integer> it = numbersSet.iterator();
+        while (it.hasNext()) {
+            int number = it.next();
+            if (isPrime(number))
+                count++;
+        }
+        return count;
+    }
+    
+    public boolean isPrime(int num) {
+        if (num == 0 || num == 1)
+            return false;
+        int lim = (int)Math.sqrt(num);
+
+        for (int i = 2; i <= lim; i++)
+            if (num % i == 0)
+                return false;
+
+        return true;
+    }
+
+    public void dfs(String str, String numbers) {
+        if (!str.equals(""))
+            numbersSet.add(Integer.valueOf(str));
+
+        for (int i = 0; i < numbers.length(); i++)
+            dfs(str + numbers.charAt(i), numbers.substring(0, i) + numbers.substring(i + 1));
+
+    }
+}
